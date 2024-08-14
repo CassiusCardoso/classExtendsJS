b92187f979c814184b9cecd1d0aa5325aa4b2227
CassiusCardoso <cassiuscardosoo@gmail.com>
2024-08-14 14:08:37 -0300

    CPF Class JS

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
-class Animal{
-    constructor(raca, cor, nome, patas, idade){
-        this._raca = raca;
-        this._cor = cor;
-        this._nome = nome;
-        this._patas = patas;
-        this._idade = idade;
+class CPFValidator {
+    constructor(cpf) {
+        this._cpf = cpf.replace(/\[^\d]+/g, '')  //Remove os caracteres especiais
     }
-    
-    comer(){
-        console.log(`${this._raca} ${this._nome} come o seu whiskas`)
-    }
-}
+    // Método principal para verificar se o CPF é válido
+    isValid() {
+        if (!this.hasValidLength() || this.hasAllSameDigits()) return false;
 
-class Cachorro extends Animal{ 
-    
-        latir(){
-            console.log(`${this._raca} está rital`)
+        const digito1 = this.firstNumberVerify();
+        const digito2 = this.secondNumberVerify();
+
+        return this.verifyNumbers(digito1, digito2);
+    }
+
+    // Verifica se o CPF tem 11 dígitos
+    hasValidLength() {
+        return this._cpf.length === 11;
+    }
+
+    // Verifica se todos os dígitos do CPF são iguais
+    hasAllSameDigits() {
+        const firstNumber = cpf[0];
+        return this.cpf.split('').every(number => number === firstNumber)
+    }
+
+    // Calcula o primeiro dígito verificador
+
+    firstNumberVerify() {
+        let soma = 0;
+        for (let i = 0; i < 9; i++) {
+            soma += parseInt(this.cpf.charAt(i)) * (10 - i);
         }
-}
 
-class Peixe extends Animal{
-    nadar(){
-        console.log(`${this._raca} nada`)
+        const resto = 11 - (soma % 11);
+        return (resto === 10) || (resto === 11) ? 0 : resto;
+
+    }
+
+    secondNumberVerify() {
+        let soma = 0;
+        for (let i = 0; i < 10; i++){
+            soma += parseInt(this.cpf.charAt(i)) * (11 - i);
+        }
+
+        const resto = 11 - (soma % 11);
+        return (resto === 10) || (resto === 11) ? 0 : resto;
+    }
+
+    // Verifica os dígitos calculados com os dígitos do CPF
+    verifyNumbers(digito1, digito2){
+        return digito1 === parseInt(this.cpf.charAt(9)) && digito2 === parseInt(this.cpf.charAt(10));
     }
 }
 
-const viraLata = new Cachorro('vira-lata', 'caramelo', 'Antônio', 4, 2)
-const peixota = new Peixe('baiacu', 'amarelo', 'Flex', 0, 1)
-
-viraLata.latir()
-viraLata.comer()
-peixota.comer()
-peixota.nadar()
-console.log(viraLata, peixota)
+const cpfValidator = new CPFValidator('123456789-02')
+console.log(cpfValidator.isValid())
